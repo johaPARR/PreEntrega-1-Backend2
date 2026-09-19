0c8a0b6e9fb6408c7f315e709c283eecb14e79d7
Joha Parrello <aylala30@gmail.com>
2026-09-19 09:59:51 -0300

    chore: eliminar auth.middleware y verifyToken en desuso

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,8 +5,3 @@ export const generateToken = (payload) => {
     const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
     return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 };
-
-export const verifyToken = (token) => {
-    const JWT_SECRET = process.env.JWT_SECRET;
-    return jwt.verify(token, JWT_SECRET);
-};
